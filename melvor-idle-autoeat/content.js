66f124ed5d0efe5a6c3973b3ea345fda3d4332f6
Jovylle Bermudez CMD <jovyllebermude.z@gmail.com>
2023-09-21 23:52:08 +0800

    clean up and optimize

--- melvor-idle-autoeat/content.js
+++ melvor-idle-autoeat/content.js
@@ -1,50 +1,67 @@
 (() => {
 
-  // Function to click the element
-  function autoEatHandler () {
-    
-    const maxElement = document.querySelector("#combat-player-hitpoints-max-1")
-    const currentElement = document.querySelector("#combat-player-hitpoints-current-1")
-    const eatElement = document.querySelector("#combat-footer-minibar-eat-btn")
-    if (maxElement && currentElement && eatElement) {
+  const maxElementId = "#combat-player-hitpoints-max-1"
+  const currentElementId = "#combat-player-hitpoints-current-1"
+  const eatElementId = "#combat-footer-minibar-eat-btn"
 
+  const interval = 2000 // 2000 milliseconds = 2 seconds
+  const heathRatio = 0.7 // 70% health, eat when health is below this
 
-      const max = parseInt(maxElement.textContent)
-
-      const current = parseInt(currentElement.textContent)
-
-      const ratio = current / max;
-
-      if (ratio < 0.8) {
-        eatElement.click();
-        console.log("hp low eating");
-      } else {
-        console.log("hp high");
-      }
+  let eatElement = document.querySelector(eatElementId)
+  function startEating () {
+    if (isReadyToEat()) {
+      console.log("AutoEat eating!");
+      eatElement.click();
+    } else {
+      // console.log("AutoEat no ready to eat!");
     }
   }
-  function theTick () {
-    console.log("AutoEat is ticking!")
-    autoEatHandler();
+
+  function isReadyToEat () {
+
+    const maxElement = document.querySelector(maxElementId)
+    const currentElement = document.querySelector(currentElementId)
+    eatElement = document.querySelector(eatElementId)
+
+    if (maxElement && currentElement && eatElement) {
+
+      // console.log(currentElement.textContent + "is current life")
+      // console.log(maxElement.textContent + "is max life")
+
+      const max = parseInt(maxElement.textContent)
+      const current = parseInt(currentElement.textContent)
+      const ratio = current / max;
+      if (ratio < heathRatio) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
   }
 
-  function tickHandler () {
-    // Set up an interval to click the button every minute (60000 milliseconds)
-    setInterval(theTick, 3000); // 5000 milliseconds = 5 seconds
+  function startLoopCheck () {
+    // console.log("AutoEat start the loop check!");
+    setInterval(() => { // 2000 milliseconds = 2 seconds
+      startEating();
+    }, interval);
   }
 
   function checkIfGmaeLoaded () {
     const loaderElement = document.querySelectorAll("#m-page-loader.d-none");
     if (loaderElement.length > 0) {
-      // debugger
       // if the variables are ready. start checking for loot to loot
-      tickHandler();
+      startLoopCheck();
     } else {
       // Continue checking until the variable is defined
       setTimeout(checkIfGmaeLoaded, 2000); // Check every 2 seconds
     }
   }
+
   // Start checking for the variable
+  console.log("AutoEat started!");
   checkIfGmaeLoaded();
+
 
 })();
