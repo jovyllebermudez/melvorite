cf7fac9a359bf8db75ea61b70405eef1c6b63d73
jovyllebermudez <jovyllebermudez@gmail.com>
2023-09-27 02:03:24 +0800

    test

--- melvor-idle-autoloot/content.js
+++ melvor-idle-autoloot/content.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 (() => {
-  // const lootButton1 = "#combat-loot button.btn.btn-sm.btn-success";
+  // test change
+  // const lootButton1 = "#combat-loot button.btn.btn-sm.btn-success"; 
   const lootButton2 = '#combat-loot lang-string[lang-id="COMBAT_MISC_46"]';
   const interval = 60000 // 60000 milliseconds = 60 seconds
 
